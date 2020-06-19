4e44f72a6c4e8d382d19563bd05b3fbd8d314ab6
ShivamKumarJha <jha.shivam3@gmail.com>
2020-06-19 21:10:26 +0530

    Add JsonUtility. Thanks to my UserList app
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ app/src/main/java/com/shivamkumarjha/bookstore/utility/JsonUtility.kt
@@ -0,0 +1,55 @@
+package com.shivamkumarjha.bookstore.utility
+
+import android.util.Log
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import com.shivamkumarjha.bookstore.model.Book
+import java.io.*
+
+private const val TAG = "JSONUtility"
+
+class JsonUtility(private val file: File) {
+    private val gson = Gson()
+
+    fun fileExists(): Boolean {
+        if (!file.exists())
+            return false
+        return true
+    }
+
+    private fun readFromFile(): String? {
+        var jsonString: String? = null
+        try {
+            val inputStream: FileInputStream = file.inputStream()
+            val inputStreamReader = InputStreamReader(inputStream)
+            val bufferedReader = BufferedReader(inputStreamReader)
+            jsonString = bufferedReader.use { it.readText() }
+            inputStream.close()
+        } catch (e: FileNotFoundException) {
+            Log.e(TAG, "File not found: $e")
+        } catch (e: IOException) {
+            Log.e(TAG, "Can not read file: $e")
+        }
+        return jsonString
+    }
+
+    fun writeToFile(books: ArrayList<Book>) {
+        val data = gson.toJson(books)
+        try {
+            val outputStreamWriter = OutputStreamWriter(file.outputStream())
+            outputStreamWriter.write(data)
+            outputStreamWriter.close()
+        } catch (e: IOException) {
+            Log.e(TAG, "File write failed: $e")
+        }
+    }
+
+    fun getBooks(): ArrayList<Book> {
+        if (!fileExists())
+            return ArrayList()
+        // read JSON file storing array of Details object
+        val jsonString = readFromFile()
+        val detailsTypeToken = object : TypeToken<List<Book>>() {}.type
+        return gson.fromJson(jsonString, detailsTypeToken)
+    }
+}
