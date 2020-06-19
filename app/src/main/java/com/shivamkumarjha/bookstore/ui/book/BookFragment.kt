04e3525794c90f95775183b2aac7774f8ca5232a
ShivamKumarJha <jha.shivam3@gmail.com>
2020-06-20 00:26:43 +0530

    Refactor. Setup Book recyclerView
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- app/src/main/java/com/shivamkumarjha/bookstore/ui/book/BookFragment.kt
+++ app/src/main/java/com/shivamkumarjha/bookstore/ui/book/BookFragment.kt
@@ -4,19 +4,24 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.shivamkumarjha.bookstore.R
+import com.shivamkumarjha.bookstore.model.Book
+import com.shivamkumarjha.bookstore.ui.adapter.BookAdapter
+import com.shivamkumarjha.bookstore.ui.adapter.BookItemClickListener
 import com.shivamkumarjha.bookstore.utility.JsonUtility
 import java.io.File
 
-class BookFragment : Fragment() {
+class BookFragment : Fragment(), BookItemClickListener {
 
     private lateinit var bookViewModel: BookViewModel
-    private val textView by lazy {
-        requireView().findViewById<TextView>(R.id.text_home)
+    private lateinit var bookAdapter: BookAdapter
+    private val recyclerView by lazy {
+        requireView().findViewById<RecyclerView>(R.id.book_recycler_view)
     }
 
     override fun onCreateView(
@@ -29,22 +34,29 @@ class BookFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setUpJSON()
         setUpViewModel()
+        setUpRecyclerView()
+    }
+
+    override fun onBookClick(book: Book) {
+        Toast.makeText(requireContext(), book.title, Toast.LENGTH_LONG).show()
     }
 
     private fun setUpViewModel() {
         bookViewModel =
             ViewModelProvider(this).get(BookViewModel::class.java)
-        bookViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
     }
 
-    private fun setUpJSON() {
-        val fileName = "Books.json"
-        val file = File(requireActivity().filesDir, fileName)
+    private fun setUpRecyclerView() {
+        bookAdapter = BookAdapter(getBooks(), this)
+        recyclerView.adapter = bookAdapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        recyclerView.setHasFixedSize(true)
+    }
+
+    private fun getBooks(): ArrayList<Book> {
+        val file = File(requireActivity().filesDir, "Books.json")
         val jsonUtility = JsonUtility(file)
-        jsonUtility.getBooks()
+        return jsonUtility.getBooks()
     }
 }
--- app/src/main/java/com/shivamkumarjha/bookstore/ui/book/BookViewModel.kt
+++ app/src/main/java/com/shivamkumarjha/bookstore/ui/book/BookViewModel.kt
@@ -1,13 +1,5 @@
 package com.shivamkumarjha.bookstore.ui.book
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class BookViewModel : ViewModel() {
-
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is book Fragment"
-    }
-    val text: LiveData<String> = _text
-}
+class BookViewModel : ViewModel()
