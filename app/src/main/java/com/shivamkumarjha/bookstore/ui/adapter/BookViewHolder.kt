37ad75aef9689ff589d8b85016a8bb47a6dc6efa
ShivamKumarJha <jha.shivam3@gmail.com>
2020-06-20 00:05:31 +0530

    Refactor. Add BookViewHolder
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ app/src/main/java/com/shivamkumarjha/bookstore/ui/adapter/BookViewHolder.kt
@@ -0,0 +1,34 @@
+package com.shivamkumarjha.bookstore.ui.adapter
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.shivamkumarjha.bookstore.R
+import com.shivamkumarjha.bookstore.model.Book
+
+class BookViewHolder(itemView: View, clickListener: BookItemClickListener) :
+    RecyclerView.ViewHolder(itemView) {
+    private val bookImage: ImageView = itemView.findViewById(R.id.book_image_view_id)
+    private val bookTitle: TextView = itemView.findViewById(R.id.book_text_view_title)
+    private val bookAuthor: TextView = itemView.findViewById(R.id.book_text_view_author)
+    private val bookPrice: TextView = itemView.findViewById(R.id.book_text_view_price)
+    private val bookRating: TextView = itemView.findViewById(R.id.book_text_view_rating)
+    private lateinit var book: Book
+
+    init {
+        itemView.setOnClickListener {
+            clickListener.onBookClick(book)
+        }
+    }
+
+    fun initialize(book: Book) {
+        this.book = book
+        // TODO use glide to get book cover from URL
+        bookImage.setBackgroundResource(R.mipmap.ic_launcher)
+        bookTitle.text = book.title
+        bookAuthor.text = book.author
+        bookPrice.text = book.price.toString()
+        bookRating.text = book.rating
+    }
+}
