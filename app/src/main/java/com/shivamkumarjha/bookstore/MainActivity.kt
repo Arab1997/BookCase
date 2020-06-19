0ff88125082d3df4f1ba7b58c78f84cf4bf04333
ShivamKumarJha <jha.shivam3@gmail.com>
2020-06-19 17:29:25 +0530

    Refactor. Add own toolbar
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- app/src/main/java/com/shivamkumarjha/bookstore/MainActivity.kt
+++ app/src/main/java/com/shivamkumarjha/bookstore/MainActivity.kt
@@ -2,28 +2,31 @@ package com.shivamkumarjha.bookstore
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var toolbar: Toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setUpNavigation()
+        setUpToolbar()
+        setUpBottomNavigationView()
     }
 
-    private fun setUpNavigation() {
+    private fun setUpBottomNavigationView() {
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration =
-            AppBarConfiguration(setOf(R.id.navigation_book, R.id.navigation_cart))
-        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+
+    private fun setUpToolbar() {
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = resources.getString(R.string.app_name)
     }
 }
