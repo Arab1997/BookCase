f7e8b9e8935f3d3d98e7793a164c900045c90ed7
ShivamKumarJha <jha.shivam3@gmail.com>
2020-06-20 00:14:50 +0530

    Refactor. Add Filter method in BookAdapter
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- app/src/main/java/com/shivamkumarjha/bookstore/ui/adapter/BookAdapter.kt
+++ app/src/main/java/com/shivamkumarjha/bookstore/ui/adapter/BookAdapter.kt
@@ -2,14 +2,20 @@ package com.shivamkumarjha.bookstore.ui.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
 import androidx.recyclerview.widget.RecyclerView
 import com.shivamkumarjha.bookstore.R
 import com.shivamkumarjha.bookstore.model.Book
+import java.util.*
+import kotlin.collections.ArrayList
 
 class BookAdapter(
     private var books: ArrayList<Book>,
     private val clickListener: BookItemClickListener
-) : RecyclerView.Adapter<BookViewHolder>() {
+) : RecyclerView.Adapter<BookViewHolder>(), Filterable {
+
+    private val backupList = books
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookViewHolder {
         val itemView =
@@ -23,5 +29,39 @@ class BookAdapter(
 
     override fun onBindViewHolder(holder: BookViewHolder, position: Int) {
         holder.initialize(books[position])
+    }
+
+    override fun getFilter(): Filter {
+        return object : Filter() {
+
+            override fun performFiltering(charSequence: CharSequence?): FilterResults {
+                var filteredList: ArrayList<Book>? = ArrayList()
+                if (charSequence.toString().isEmpty()) {
+                    filteredList = backupList
+                } else {
+                    for (book in books) {
+                        if (book.title.toLowerCase(Locale.ROOT)
+                                .contains(charSequence.toString().toLowerCase(Locale.ROOT)) ||
+                            book.author.toLowerCase(Locale.ROOT)
+                                .contains(charSequence.toString().toLowerCase(Locale.ROOT))
+                        ) {
+                            filteredList?.add(book)
+                        }
+                    }
+                }
+                val filterResults = FilterResults()
+                filterResults.values = filteredList
+                return filterResults
+            }
+
+            @Suppress("UNCHECKED_CAST")
+            override fun publishResults(
+                charSequence: CharSequence?,
+                filterResults: FilterResults?
+            ) {
+                books = filterResults?.values as ArrayList<Book>
+                notifyDataSetChanged()
+            }
+        }
     }
 }
