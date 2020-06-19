98aafe7be0ae0e777f72de6af1f2941033e8def6
ShivamKumarJha <jha.shivam3@gmail.com>
2020-06-19 18:57:54 +0530

    Refactor. Add book item layout
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- app/src/main/java/com/shivamkumarjha/bookstore/model/Book.kt
+++ app/src/main/java/com/shivamkumarjha/bookstore/model/Book.kt
@@ -1,10 +1,15 @@
 package com.shivamkumarjha.bookstore.model
 
+import android.widget.ImageView
+
 data class Book(
     val bookID: Int,
     val title: String,
     val author: String,
     val description: String,
+    val displayImage: ImageView,
+    val image: ImageView,
+    val category: String,
     val rating: String,
     val review: String,
     val price: Int,
