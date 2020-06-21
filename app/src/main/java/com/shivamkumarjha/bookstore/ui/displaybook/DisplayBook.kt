ca5175cb5d81dfa00f2e00580adcd5da67576c42
ShivamKumarJha <jha.shivam3@gmail.com>
2020-06-21 20:41:39 +0530

    Refactor. Update display book layout
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- app/src/main/java/com/shivamkumarjha/bookstore/ui/displaybook/DisplayBook.kt
+++ app/src/main/java/com/shivamkumarjha/bookstore/ui/displaybook/DisplayBook.kt
@@ -1,9 +1,12 @@
 package com.shivamkumarjha.bookstore.ui.displaybook
 
+import android.annotation.SuppressLint
+import android.graphics.Paint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
@@ -11,13 +14,18 @@ import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import com.shivamkumarjha.bookstore.R
 import com.shivamkumarjha.bookstore.model.Book
+import com.squareup.picasso.Picasso
 
 class DisplayBook(private val book: Book) : Fragment() {
 
     private lateinit var toolbar: Toolbar
-    private val textView by lazy {
-        requireView().findViewById<TextView>(R.id.text_view_display_book)
-    }
+    private lateinit var bookImage: ImageView
+    private lateinit var bookTitle: TextView
+    private lateinit var bookAuthor: TextView
+    private lateinit var bookDescription: TextView
+    private lateinit var bookPrice: TextView
+    private lateinit var bookMRP: TextView
+    private lateinit var bookDiscount: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,7 +38,7 @@ class DisplayBook(private val book: Book) : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         setUpToolBar()
         backPressDispatcher()
-        textView.text = book.title
+        setUpViews()
     }
 
     override fun onResume() {
@@ -45,8 +53,35 @@ class DisplayBook(private val book: Book) : Fragment() {
 
     private fun setUpToolBar() {
         toolbar = requireView().findViewById(R.id.display_book_toolbar_id)
+        toolbar.title = book.title
         toolbar.setNavigationIcon(R.drawable.ic_back)
         toolbar.setNavigationOnClickListener { exitFragment() }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun setUpViews() {
+        // init view items
+        bookImage = requireView().findViewById(R.id.display_book_image)
+        bookTitle = requireView().findViewById(R.id.display_book_title_text_view_id)
+        bookAuthor = requireView().findViewById(R.id.display_book_author_text_view_id)
+        bookDescription = requireView().findViewById(R.id.display_book_description_text_view_id)
+        bookPrice = requireView().findViewById(R.id.display_book_text_view_price)
+        bookMRP = requireView().findViewById(R.id.display_book_text_view_mrp)
+        bookDiscount = requireView().findViewById(R.id.display_book_text_view_discount)
+
+        // set value
+        Picasso.get().load(book.imageLink)
+            .resize(500, 0).centerCrop()
+            .placeholder(R.mipmap.ic_launcher)
+            .error(R.mipmap.ic_launcher)
+            .into(bookImage)
+        bookTitle.text = book.title
+        bookAuthor.text = "By ${book.author}"
+        bookDescription.text = book.description
+        bookPrice.text = "Rs " + book.price * 76.25f // Price USD to INR
+        bookMRP.text = "Rs " + book.maximumRetailPrice * 76.25f // Price USD to INR
+        bookMRP.paintFlags = Paint.STRIKE_THRU_TEXT_FLAG
+        bookDiscount.text = book.discount.toInt().toString() + "% off"
     }
 
     private fun backPressDispatcher() {
